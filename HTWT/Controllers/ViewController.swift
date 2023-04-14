fdf30eda6a21dc797f4711a5ec06485bf6377c5c
B-string <brightstring134@gmail.com>
2023-04-14 22:57:38 +0900

    2023/04/14 commit

--- HTWT/Controllers/ViewController.swift
+++ HTWT/Controllers/ViewController.swift
@@ -9,14 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
 
+    // MARK: - setup
+    func setupTableView() {
+        tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: "CurrentWeatherCell", bundle: nil),
+                           forCellReuseIdentifier: "CurrentWeatherCell")
+    }
 
 }
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(#function)
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CurrentWeatherCell", for: indexPath) as! CurrentWeatherCell
+            cell.selectionStyle = .none
+            
+            tableView.rowHeight = 355
+            
+            return cell
+        default:
+            let cell = UITableViewCell()
+            return cell
+            
+        }
+    }
+    
+    
+    
+}
+
 
---
+++ HTWT/Models/HTWTManager.swift
@@ -0,0 +1,15 @@
+//
+//  HTWTManager.swift
+//  HTWT
+//
+//  Created by Seokhyeon Jeong on 2023/04/14.
+//
+
+import Foundation
+
+class HTWTManager {
+    
+    static let shared = HTWTManager()
+    
+    
+}
